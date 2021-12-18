25aa72870a6443100de9a18760b1e309f6d63fe7
Ayush Sharma <ayush.sharma@pristyncare.com>
2021-12-19 03:27:42 +0530

    chat bot uii

---
+++ assets/js/chatBot.js
@@ -0,0 +1,9 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+    document.getElementById("chat-bot").style.display = "none";
+  }
+  
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+    document.getElementById("chat-bot").style.display = "block";
+}
